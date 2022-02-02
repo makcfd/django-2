c139b3ba9872380bd9c8520927e306a20daf20d0
fediushkin <fediushkin@fim.uni-passau.de>
2022-02-03 00:48:05 +0100

    deployment files and settings added

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#7r4)a8wu)=@#1lrtncdn(9c6ms5y3hx+tr^$n=j!4v4+nm382
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
